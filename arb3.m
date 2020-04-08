709b06a4d7e93d8863dd79dacbd8ffc4496c0f31
Ryan Fielding <rafieldi@sfu.ca>
2020-04-08 00:12:55 -0700

    tidy up, first .tex tables

---
+++ arb3.m
@@ -0,0 +1,132 @@
+clc;
+clear all;
+close all;
+
+%% Testing
+
+%cad, usd, eur
+%col by row means currency 1 to currency 2
+%eg. element (2,1) is cad to usd exchange rate
+cad2usd = 0.7085;
+cad2eur = 0.6559;
+cad2yen = 77.2049;
+usd2cad = 1.4113;
+usd2eur = 0.9260;
+usd2yen = 108.833;
+eur2cad = 1.52448;
+eur2usd = 1.0799;
+eur2yen = 117.618;
+yen2cad = 0.0129508;
+yen2usd = 0.00918840;
+yen2eur = 0.00850212;
+global EXrates x0
+%initial amount 10000 CAD
+x0 = 10; %thousand
+EXrates = [ 1       usd2cad eur2cad;
+            cad2usd 1       eur2usd;
+            cad2eur usd2eur 1];
+%x = x(1,1) x(1,2) x(1,3) x(1,4) x(2,1) ... x(4,4)
+%x = 0 usd2cad eur2cad yen2cad ... 0 in amounts
+x = zeros(1,9);
+
+Aeq = [0 1 0 -EXrates(2,1) 0 -EXrates(2,3) 0 1 0;
+    0 0 1 0 0 1 -EXrates(3,1) -EXrates(3,2) 0];
+beq = [0; 0];
+A = [];
+b = [];
+
+xGuess = [0 10 10 10 0 10 10 10 0];
+
+lb = [0,0,0,0,0,0,0,0,0];
+ub = [0,10,10,10,0,10,10,10,0];
+[x, fval, exit, out] = fmincon(@optimize,xGuess,A,b,Aeq,beq,lb,ub);
+
+fminconRates=EXrates
+T = table(x','VariableNames',{'fminconOptima'})
+profit = -fval-x0
+gain = profit/x0
+
+
+opts = optimoptions('ga');
+opts.FunctionTolerance = 1E-6;
+lb = [0,0,0,1,0,0,0,0,0];
+ub = [0,10,10,15,0,10,15,10,0];
+[x2, fval2, exit2, out2] = ga(@optimize,9,A,b,Aeq,beq,lb,ub,[],opts);
+
+gaRates=EXrates
+T2 = table(x2','VariableNames',{'gaOptima'})
+profit2 = -fval2-x0
+gain2 = profit2/x0
+
+%% Functions
+
+function f = optimize(x)
+    global EXrates x0
+    %arbitrage formulation
+    %initial amount + conversions from other currencies = final amount +
+    %conversions to other currencies
+    f = -(x0 + (EXrates(1,2)*x(2) + EXrates(1,3)*x(3)) - (x(4) + x(7)));
+end
+
+%% Results
+% Local minimum found that satisfies the constraints.
+% 
+% Optimization completed because the objective function is non-decreasing in 
+% feasible directions, to within the value of the optimality tolerance,
+% and constraints are satisfied to within the value of the constraint tolerance.
+% 
+% <stopping criteria details>
+% 
+% fminconRates =
+% 
+%     1.0000    1.4113    1.5245
+%     0.7085    1.0000    1.0799
+%     0.6559    0.9260    1.0000
+% 
+% 
+% T =
+% 
+%   table
+% 
+%     fminconOptima
+%     _____________
+% 
+%     [1×9 double] 
+% 
+% 
+% profit =
+% 
+%     0.0017
+% 
+% 
+% gain =
+% 
+%    1.6696e-04
+% 
+% Optimization terminated: average change in the fitness value less than options.FunctionTolerance.
+% 
+% gaRates =
+% 
+%     1.0000    1.4113    1.5245
+%     0.7085    1.0000    1.0799
+%     0.6559    0.9260    1.0000
+% 
+% 
+% T2 =
+% 
+%   table
+% 
+%       gaOptima  
+%     ____________
+% 
+%     [1×9 double]
+% 
+% 
+% profit2 =
+% 
+%    -0.0016
+% 
+% 
+% gain2 =
+% 
+%   -1.6003e-04
--- arb3varyPractical.m
+++ arb3varyPractical.m
@@ -4,6 +4,11 @@ close all;
 
 %% Testing
 
+%transfer fee polyfits
+global pUSD
+global pCAD
+[pUSD, pCAD] = fees();
+
 %cad, usd, eur
 %col by row means currency 1 to currency 2
 %eg. element (2,1) is cad to usd exchange rate
@@ -13,17 +18,16 @@ usd2cad = 1.4113;
 usd2eur = 0.9260;
 eur2cad = 1.52448;
 eur2usd = 1.0799;
-
 global EXrates x0
 %initial amount 10000 CAD
-x0 = 10; %thousand
+x0 = 100; %thousand
 EXrates = [ 1       usd2cad eur2cad;
             cad2usd 1       eur2usd;
             cad2eur usd2eur 1];
 %x = x(1,1) x(1,2) x(1,3) x(1,4) x(2,1) ... x(3,3)
-%x(10-12) are the rates
+%x(10-11) are the cad to other currency rates
 %x = 0 usd2cad eur2cad yen2cad ... 0 in amounts
-x = zeros(1,12);
+x = zeros(1,11);
 
 Aeq = [];
 beq = [];
@@ -31,17 +35,14 @@ A = [];
 b = [];
 
 %finds 0.0048 profit
-%xGuess = [0 10 10 10 0 10 10 10 0 0.713554 0.66 .9];
-%finds 1.56 profit
-%xGuess=[0,1.45956047181335,4.36166553397463,7.27194070618012,0,5.64212289465898,0.00552664825793103,9.99999998644946,0,0.799999995962151,0.600000004715667,0.999999986584457];
-%finds 1.147
-%xGuess=[0,9.99242501921721,1.60319216391258e-06,1.79994172185933,0,8.85986264219077,9.99999984985734,1.60089391853419,0,0.650000000000000,0.749999981783946,0.850000000000000];
-%finds 0.3819
-xGuess=[0,8.75266172520550,2.32746313743225,8.64661087348664,0,6.68752910409166,7.12717579347866,4.84865467546798,0,0.680176110219948,0.675591051281899,0.866237297766703];
+xGuess = [0 10 10 10 0 10 10 10 0 0.713554 0.66];
+
 
 nonlcon=@varyRates;
-lb = [0,0,0,0,0,0,0,0,0, 0.65, 0.60, 0.85];
-ub = [0,10,10,10,0,10,10,10,0,0.8,0.75,1.0];
+%trade bounds and rate bounds
+%rate bounds based off 5year h/l for cad to usd / eur
+lb = [0,0,0,0,0,0,0,0,0,0.68250,0.61961];
+ub = [0,10,10,10,0,10,10,10,0,0.83642,0.76188];
 [x, fval, exit, out] = fmincon(@optimize,xGuess,A,b,Aeq,beq,lb,ub,nonlcon);
 
 fminconRates=EXrates
@@ -49,27 +50,18 @@ T = table(x','VariableNames',{'fminconOptima'})
 profit = -fval-x0
 gain = profit/x0
 
-FMResults(fval, profit, gain, out.iterations, out.funcCount,'FMResults');
-RatesResults(fminconRates,'fminconRates');
 
 opts = optimoptions('ga');
 opts.FunctionTolerance = 1E-6;
 opts.ConstraintTolerance = 1E-6;
 %ensures the losses at each other currency (usd or eur) is 10 cents max
-[x2, fval2, exit2, out2] = ga(@optimize,12,A,b,Aeq,beq,lb,ub,nonlcon,opts);
+[x2, fval2, exit2, out2] = ga(@optimize,11,A,b,Aeq,beq,lb,ub,nonlcon,opts);
 
 gaRates=EXrates
 T2 = table(x2','VariableNames',{'gaOptima'})
 profit2 = -fval2-x0
 gain2 = profit2/x0
 
-GAResults(fval2, profit2, gain2, out2.generations, out2.funccount,'GAResults');
-RatesResults(gaRates,'gaRates');
-
-%%Copy Latex files to folder
-%copyfile P* Report/figures
-
-%% Functions
 
 function f = optimize(x)
     global EXrates x0
@@ -79,22 +71,26 @@ function f = optimize(x)
     f = -(x0 + (EXrates(1,2)*x(2) + EXrates(1,3)*x(3)) - (x(4) + x(7)));
 end
 
+function f = prof(x)
+    f = (x(1)-usdFee(x(1)))*x(3) + (x(2) - cadFee(x(2)))/x(4) - 2*x(1); %in USD
+end
+
 function [c,ceq]=varyRates(x)
    
     cad2usd = x(10);
     cad2eur = x(11);
-    usd2eur = x(12);
-    %usd2eur = (1/cad2usd)*cad2eur is more realistic / practical to the
-    %market
 
     usd2cad = 1/cad2usd;
     eur2cad = 1/cad2eur;
+    usd2eur = (1/cad2usd)*cad2eur;
+    %is more realistic / practical to the market, since not all 3 rates can
+    %vary independently?
     eur2usd = 1/usd2eur;
-    
+
     global EXrates
     EXrates = [ 1       usd2cad eur2cad;
-            cad2usd 1       eur2usd;
-            cad2eur usd2eur 1];
+                cad2usd 1       eur2usd;
+                cad2eur usd2eur 1];
             
     %old linear constraint is now non linear since vary 3 exch rates
     % Aeq = [0 1 0 -EXrates(2,1) 0 -EXrates(2,3) 0 1 0;
@@ -111,29 +107,37 @@ function [c,ceq]=varyRates(x)
     c(1) = -((EXrates(1,2)*x(2) + EXrates(1,3)*x(3)) - (x(4) + x(7)));
 end
 
-function FMResults(opt, profit, gain, iters, funcVals, name)
-    Res.Optimum = opt;
-    Res.Profit = profit;
-    Res.Gain = gain;
-    Res.Iterations = iters.';
-    Res.FuncEvals = funcVals.';
-    Res = struct2table(Res);
-    table2latex(Res,name);
-end
-function GAResults(opt, profit, gain, gens, funcVals, name)
-    Res.Optimum = opt;
-    Res.Profit = profit;
-    Res.Gain = gain;
-    Res.Generations = gens.';
-    Res.FuncCount = funcVals.';
-    Res = struct2table(Res);
-    table2latex(Res,name);
+function fee = usdFee(val)
+    %Fee to send USD to CAD
+    global pUSD
+    fee = polyval(pUSD,val);
 end
 
-function RatesResults(rates,name)
-    Res.CAD = rates(:,1);
-    Res.USD = rates(:,2);
-    Res.EUR = rates(:,3);
-    Res = struct2table(Res);
-    table2latex(Res,name);
+function fee = cadFee(val)
+    %Fee to send USD to CAD
+    global pCAD
+    fee = polyval(pCAD,val);
+end
+
+function [p1, p2] = fees()
+    %% Poly fit fee data
+    x = [100 500 1000 5000 10000 15000]';
+    yUSD = [1.99 5.48 9.86 44.82 88.53 132.24]';
+    yCAD = [3.22 6.95 11.61 48.92 78.84 114.58]';
+
+    p1 = polyfit(x,yUSD,4);
+    
+    p2 = polyfit(x,yCAD,4);
+    
+    feeUSD = polyval(p1,x);
+    feeCAD = polyval(p2,x);
+    
+%     figure
+%     plot(x,yCAD,'o')
+%     hold on
+%     plot(x,yUSD,'r*')
+%     plot(x,feeUSD,'r-')
+%     plot(x,feeCAD,'b-')
+%     axis([0  15000  0  150])
+%     hold off
 end
